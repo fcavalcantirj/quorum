7bb9f9c0f9c0b9212bab09e277837c17511729e9
fcavalcantirj <felipe.cavalcanti.rj@gmail.com>
2026-03-22 21:50:55 -0300

    style(01-03): replace interface{} with any

--- relay/internal/handler/room.go
+++ relay/internal/handler/room.go
@@ -187,7 +187,7 @@ func (h *RoomHandler) ListPublicRooms(w http.ResponseWriter, r *http.Request) {
 }
 
 // writeJSON writes a JSON response with the given status code.
-func writeJSON(w http.ResponseWriter, status int, v interface{}) {
+func writeJSON(w http.ResponseWriter, status int, v any) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	json.NewEncoder(w).Encode(v)
